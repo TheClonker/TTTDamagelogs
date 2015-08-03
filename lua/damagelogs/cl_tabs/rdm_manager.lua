5552d133f7f1173bece18e885a480176efa39178
Philip Gisella <philip.gisella@googlemail.com>
2015-08-03 19:31:30 +0200

    Fixed Report Manager Derp

--- lua/damagelogs/cl_tabs/rdm_manager.lua
+++ lua/damagelogs/cl_tabs/rdm_manager.lua
@@ -548,9 +548,11 @@ function Damagelog:DrawRDMManager(x,y)
 			local attacker = player.GetBySteamID(Damagelog.SelectedReport.attacker)
 			if not IsValid(attacker) and (Damagelog.SelectedReport.status == RDM_MANAGER_WAITING_FOR_ATTACKER or Damagelog.SelectedReport.status == RDM_MANAGER_WAITING_FOR_VICTIM) then
 				DrawStatusMenuOption(RDM_MANAGER_PROGRESS, menu)
+				DrawStatusMenuOption(RDM_MANAGER_FINISHED, menu)
 			end
 			if Damagelog.SelectedReport.status == RDM_MANAGER_WAITING then
 				DrawStatusMenuOption(RDM_MANAGER_PROGRESS, menu)
+				DrawStatusMenuOption(RDM_MANAGER_FINISHED, menu)
 			end
 			if Damagelog.SelectedReport.status == RDM_MANAGER_PROGRESS then
 				if IsValid(attacker) then
